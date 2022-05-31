c74d37e032669e1e4ae44d528913fe0cf4ba50ba
Alexey Ocheretenkp <b8er2la@outlook.com>
2022-05-31 14:40:28 +0300

    code clean; slide space-between removed

--- script/swiper-init.js
+++ script/swiper-init.js
@@ -1,26 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        // direction: 'vertical',
         loop: true,
-      
-        // If we need pagination
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
         },
 
-        spaceBetween: 30,
+        autoplay: {
+          delay: 5000,
+        },
 
-        // // Navigation arrows
-        // navigation: {
-        //   nextEl: '.swiper-button-next',
-        //   prevEl: '.swiper-button-prev',
-        // },
-      
-        // // And if we need scrollbar
-        // scrollbar: {
-        //   el: '.swiper-scrollbar',
-        // },
       });
 });
