0e784c637f54ccf1e7dccc0c461073f304146888
Alexey Ocheretenkp <b8er2la@outlook.com>
2022-06-02 20:48:22 +0300

    faq accordion:focus fixed

--- script/accordion.js
+++ script/accordion.js
@@ -3,9 +3,15 @@ $( function() {
       heightStyle: "content",
       collapsible: true,
       icons: false,
-      activate: function(event, ui) {
+      beforeActivate: function(event, ui) {
         a = ui;  
         console.log(ui);
+        
+        ui.oldHeader.parent().next().removeClass('faq__tophighlight');
+        ui.oldHeader.parent().removeClass('faq__tophighlight');
+
+        ui.newHeader.parent().next().addClass('faq__tophighlight');
+        ui.newHeader.parent().addClass('faq__tophighlight');
       }
     });
   } );
