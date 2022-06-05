3a2ca7f09a96515bb0f71fb21b0334d85f9c3a22
Alexey Ocheretenkp <b8er2la@outlook.com>
2022-06-05 12:52:56 +0300

    burger-menu adaptive

--- script/top-nav-burger.js
+++ script/top-nav-burger.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded',
 function() {
     document.querySelector('.top-nav__close-btn').addEventListener('click', toggleTopNav);
     document.querySelector('.header__burger').addEventListener('click', toggleTopNav);
+    window.addEventListener('resize', resizeTopNav);
 });
 
 function toggleTopNav() {
+    resizeTopNav();
     document.querySelector('.top-nav__list').classList.toggle('top-nav__list--active');
 };
+
+function resizeTopNav() {
+    var h = document.querySelector('.hero').clientHeight + document.querySelector('.header').clientHeight + 1;
+    document.querySelector('.top-nav__list').style.height = h + 'px';
+}
